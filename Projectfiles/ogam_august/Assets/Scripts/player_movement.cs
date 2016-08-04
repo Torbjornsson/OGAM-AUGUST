81e08d29ff6131283d713a025fa88e643e4c66aa
Torbjornsson <david.torbjornsson@gmail.com>
2016-08-04 17:22:12 +0200

    create basic playermovement

--- Projectfiles/ogam_august/Assets/Scripts/player_movement.cs
+++ Projectfiles/ogam_august/Assets/Scripts/player_movement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class player_movement : MonoBehaviour {
 
+	public KeyCode up, down, left, right;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +12,21 @@ public class player_movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+		if(Input.GetKeyDown(up)){
+			print("moveup");
+			transform.Translate(Vector2.up);
+		}
+		if(Input.GetKeyDown(down)){
+			print("movedown");
+			transform.Translate(Vector2.down);
+		}
+		if(Input.GetKeyDown(left)){
+			print("moveleft");
+			transform.Translate(Vector2.left);
+		}
+		if(Input.GetKeyDown(right)){
+			print("moveright");
+			transform.Translate(Vector2.right);
+		}
 	}
 }
