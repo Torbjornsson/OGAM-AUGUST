935ee17d7af6c3e14e080f291e53e0e9d1b8965c
HannesHaggander <h.haggander@gmail.com>
2016-08-10 23:03:41 +0200

    added save possabilities, not tried but should work. nothing fancy

--- Projectfiles/ogam_august/Assets/Scripts/General/KeepOnLoad.cs
+++ Projectfiles/ogam_august/Assets/Scripts/General/KeepOnLoad.cs
@@ -1,17 +1,70 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class KeepOnLoad : MonoBehaviour {
 
-    public string levelToLoad = "houseHome";
+    public static KeepOnLoad masterObject;
 
-    void Start()
+    public string levelToLoad = "houseHome";
+    private string filePath = Application.persistentDataPath + "/PlayerData.dat";
+
+    public int exp = 0;
+    public int completedLevel = 0;
+    public ArrayList items = new ArrayList();
+
+    void Awake()
     {
-        if (SceneManager.GetActiveScene().name.Equals("SetupScene"))
+        if(masterObject == null)
         {
             DontDestroyOnLoad(gameObject);
-            SceneManager.LoadScene(levelToLoad);
+            masterObject = this;
+        } 
+        else if(masterObject != this)
+        {
+            Destroy(gameObject);
         }
     }
+
+    public void Save()
+    {
+        BinaryFormatter binform = new BinaryFormatter();
+        FileStream file = File.Create(filePath);
+        PlayerData playerData = new PlayerData();
+        if(playerData.PlayerExp <= 0) { playerData.PlayerExp = 0; }
+        else { playerData.PlayerExp = exp; }
+
+        if(playerData.CompletedLevel <= 0) { playerData.CompletedLevel = 0; }
+        else { playerData.CompletedLevel = this.completedLevel; }
+
+        playerData.items = this.items;
+
+        binform.Serialize(file, playerData);
+        file.Close();
+        print("Game successfully saved!");
+    }
+
+    public void Load()
+    {
+        if (!File.Exists(filePath){ print("Game file not found " + filePath); return; }
+        BinaryFormatter binform = new BinaryFormatter();
+        FileStream file = File.Open(filePath, FileMode.Open);
+        PlayerData playerData = (PlayerData) binform.Deserialize(file);
+        file.Close();
+
+        exp = playerData.PlayerExp;
+        completedLevel = playerData.CompletedLevel;
+        print("Game successfully loaded!");
+    }
+}
+
+[Serializable]
+class PlayerData
+{
+    public int PlayerExp;
+    public int CompletedLevel;
+    public ArrayList items;
 }
